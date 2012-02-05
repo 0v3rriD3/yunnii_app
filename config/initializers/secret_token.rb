bf1909da967e30dcb139d0f0cb0bacd9dfa4d062
nullbr41n3r <khalebpr@gmail.com>
2012-02-05 09:18:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YunniiApp::Application.config.secret_token = 'f425996f4495b2c4ad6bae9b59ae3655bdb8ddf48e05c5db8b63dc3d77fa4383f6b99ce9c5c7bea6cc38f3fa26fa576ae8501469934108c5db777f4b1bbb7863'
